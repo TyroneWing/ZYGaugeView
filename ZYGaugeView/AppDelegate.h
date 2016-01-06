7c4c48bc14d85ea8322a8c11abdc168a33d2433f
yi <zhengyi@tinckay.com>
2016-01-06 17:33:22 +0800

    Initial Commit

---
+++ ZYGaugeView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYGaugeView
+//
+//  Created by yi on 16/1/6.
+//  Copyright © 2016年 yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYGaugeView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYGaugeView
+//
+//  Created by yi on 16/1/6.
+//  Copyright © 2016年 yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
